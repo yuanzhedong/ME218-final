92f4b1a0f0dbcd4bc1b2155a59e273f37fcddb7d
Yuanzhe Dong <yzd@stanford.edu>
2024-10-22 16:57:25 -0700

    add led service and hal layer

---
+++ ProjectHeaders/FontStuff.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   FontStuff.h
+ * Author: Ed
+ *
+ * Created on July 14, 2021, 4:59 PM
+ */
+
+#ifndef FONTSTUFF_H
+#define	FONTSTUFF_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+uint8_t getFontLine(unsigned char data, int line_num);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FONTSTUFF_H */
+
