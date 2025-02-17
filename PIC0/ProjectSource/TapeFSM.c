e3589271d1fecdb4cf1fab9dc0b2ccd7dc2525f0
cyrusx1 <xxiang@berkeley.edu>
2025-02-16 19:57:09 -0800

    update

--- PIC0/ProjectSource/TapeFSM.c
+++ PIC0/ProjectSource/TapeFSM.c
@@ -34,6 +34,11 @@
    relevant to the behavior of this state machine
 */
 static void exitFollowing(); //the exit function for exiting the Following state back to Idle
+static void ConfigPWM_OC1();
+static void ConfigPWM_OC3();
+static void ConfigTimer2(); //time base for OC
+static void ConfigTimer3();
+static void ConfigTimer4(); //for running control loop
 /*---------------------------- Module Variables ---------------------------*/
 // everybody needs a state variable, you may need others as well.
 // type of state variable should match htat of enum in header file
@@ -41,7 +46,7 @@ static void exitFollowing(); //the exit function for exiting the Following state
 #define PWM_freq  10000 //wheel motor PWM frquency in Hz
 #define PIC_freq 20000000
 #define PIC_freq_kHz 20000
-
+#define ns_per_tick 50 //nano-sec per tick for the PBC = 1/PIC_freq
 
 #define H_bridge1A_TRIS TRISBbits.TRISB11//output
 #define H_bridge1A_LAT LATBbits.LATB11 //latch
@@ -50,8 +55,7 @@ static void exitFollowing(); //the exit function for exiting the Following state
 #define IC_TIMER_period 50000
 
 //prescalars and priorities
-#define prescalar_OC_M1 2 //for PWM for motor1
-#define prescalar_OC_M2 2 
+#define prescalar_T2 2 //for PWM for the 2 motors
 #define prescalar_T4 4 //used for running control law
 #define priority_control 5
 
@@ -91,11 +95,17 @@ static TapeState_t CurrentState;
 ****************************************************************************/
 bool InitTapeFSM(uint8_t Priority)
 {
-  
+  //TRIS and LAT for direction control pins
   H_bridge1A_TRIS = 0; //Outputs
   H_bridge1A_LAT = 0;
   H_bridge3A_TRIS = 0; //Outputs
   H_bridge3A_LAT = 0;
+  ConfigTimer2();
+  ConfigPWM_OC1();
+  ConfigPWM_OC3();
+  ConfigTimer4();
+  /********enable interrupt globally *******************/
+  __builtin_enable_interrupts();
   ES_Event_t ThisEvent;
   MyPriority = Priority;
   // put us into the Initial PseudoState
@@ -200,6 +210,130 @@ TapeState_t QueryTapeFSM(void)
 /***************************************************************************
  private functions
  ***************************************************************************/
-void exitFollowing(){
+static void exitFollowing(){
   return;
 }
+static void ConfigTimer2() {
+  // Clear the ON control bit to disable the timer. 
+  T2CONbits.ON = 0;
+  // Clear the TCS control bit to select the internal PBCLK source.
+  T2CONbits.TCS = 0;
+  // Set input clock prescale to be 1:1. 
+  T2CONbits.TCKPS = 0b000; 
+  // Clear the timer register TMR2 
+  TMR2 = 0;
+  // Load PR2 with desired 16-bit match value
+  PR2 = PIC_freq / (PWM_freq * prescalar_OC1) - 1;
+  DB_printf("PR2 is set to %d \n", PR2);
+
+  // Clear the T2IF interrupt flag bit in the IFS2 register
+  IFS0CLR = _IFS0_T2IF_MASK;
+  // Disable interrupts on Timer 2
+  IEC0CLR = _IEC0_T2IE_MASK;
+  // Hold on to turn on the timer
+  // T2CONbits.ON = 1;
+  return; 
+}
+static void ConfigPWM_OC1() {
+
+    // map OC1 to RB3
+    RPB3R = 0b0101;
+
+    //Clear OC1CON register: 
+    OC1CON = 0;
+
+    // Configure the Output Compare module for one of two PWM operation modes
+    OC1CONbits.ON = 0;         // Turn off Output Compare module
+    OC1CONbits.OCM = 0b110;    // PWM mode without fault pin
+    OC1CONbits.OCTSEL = 0;     // Use Timer2 as the time base
+
+    // Set the PWM duty cycle by writing to the OCxRS register
+    OC1RS = PR2 * 0;       // Secondary Compare Register (for duty cycle)
+    OC1R = PR2 * 0;        // Primary Compare Register (initial value)
+
+    // Turn on Timer 2 after OC2 setup
+    T2CONbits.ON = 1;
+    // Turn ON the Output Compare module
+    OC1CONbits.ON = 1;         // Enable Output Compare module
+
+    return;
+}
+
+
+static void ConfigPWM_OC3() {
+
+    // map OC3 to RB10
+    RPB10R = 0b0101;
+
+    //Clear OC3CON register: 
+    OC3CON = 0;
+
+    // Configure the Output Compare module for one of two PWM operation modes
+    OC3CONbits.ON = 0;         // Turn off Output Compare module
+    OC3CONbits.OCM = 0b110;    // PWM mode without fault pin
+    OC3CONbits.OCTSEL = 0;     // Use Timer2 as the time base
+
+    // Set the PWM duty cycle by writing to the OCxRS register
+    OC3RS = PR2 * 0;       // Secondary Compare Register (for duty cycle)
+    OC3R = PR2 * 0;        // Primary Compare Register (initial value)
+
+    // // Turn on Timer 2 after OC2 setup
+    // T2CONbits.ON = 1;
+    // Turn ON the Output Compare module
+    OC3CONbits.ON = 1;         // Enable Output Compare module
+
+    return;
+}
+
+
+static void ConfigTimer3() {
+  // Clear the ON control bit to disable the timer. 
+  T3CONbits.ON = 0;
+  // Clear the TCS control bit to select the internal PBCLK source.
+  T3CONbits.TCS = 0;
+  // Set input clock prescale to be 1:8. 
+  T3CONbits.TCKPS = 0b011; 
+  // Clear the timer register TMR2 
+  TMR3 = 0;
+  // Load PR3 with desired 16-bit match value
+  PR3 = 0xFFFF;
+
+  // Clear the T2IF interrupt flag bit in the IFS2 register
+  IFS0CLR = _IFS0_T3IF_MASK;
+  // Set the priority of the Timer 3 interrupt to 6
+  IPC3bits.T3IP = 6; 
+
+  // Enable interrupts on Timer 3
+  IEC0SET = _IEC0_T3IE_MASK;
+  // Turn on the timer 3
+  T3CONbits.ON = 1;
+  return; 
+}
+
+static void ConfigTimer4() {
+  //choose Timer4 for control ISR
+// Disable timer4 before configuring
+ T4CONbits.ON = 0;
+ // Choose the internal clock as the source
+ T4CONbits.TCS = 0;
+ // Disable external gating
+ T4CONbits.TGATE = 0;
+ // Set the prescalar to 4
+ T4CONbits.TCKPS = 0b010; 
+ // set the period according to the ck frequency and pre-scalar to get 200Hz
+PR4 = Control_interval*1000000/ns_per_tick/prescalar_T4 - 1;
+DB_printf("PR4 is set to %d \n",PR4);
+//clear the current timer count
+TMR4=0;
+T4CONbits.ON = 1;
+//enable interrupt for the timer
+//clear the interrupt flag for TIMER4
+IFS0CLR = _IFS0_T4IF_MASK;
+// Set the priority of the interrupt for TIMER4
+IPC4bits.T4IP = priority_control;
+// Enable interrupt for TIMER4
+IEC0SET = _IEC0_T4IE_MASK; 
+
+  return;
+}
+
