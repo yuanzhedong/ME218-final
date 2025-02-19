5c2cf768fd3e7e18638e4fdc34a5f9d81ac5422a
Yuanzhe Dong <yuanzhedong@gmail.com>
2025-02-18 20:18:18 -0800

    clean code

--- PIC0/build/default/production/FrameworkSource/circular_buffer_no_modulo_threadsafe.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/FrameworkSource/circular_buffer_no_modulo_threadsafe.o: \
- FrameworkSource/circular_buffer_no_modulo_threadsafe.c \
- FrameworkHeaders/circular_buffer.h
-
-FrameworkHeaders/circular_buffer.h:
