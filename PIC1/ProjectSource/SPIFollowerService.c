488f1c731869e549ca1f6019ef6b8105aeec9d8d
Yuanzhe Dong <yzd@stanford.edu>
2025-02-24 22:07:17 -0800

    update pwm

--- PIC1/ProjectSource/SPIFollowerService.c
+++ PIC1/ProjectSource/SPIFollowerService.c
@@ -56,11 +56,10 @@ ES_Event_t RunSPIFollowerService(ES_Event_t ThisEvent)
     return ReturnEvent;
 }
 
-
 void InitSPI(void)
 {
     // Step 0: Disable analog function on all SPI pins
-    
+
     // Step 1: Map SPI Inputs/Outputs to all desired pins
     TRISBbits.TRISB5 = 1;  // Set SDI1 (RB5) as input
     SDI1R = 0b0001;        // Map SDI1 to RB5
@@ -69,8 +68,7 @@ void InitSPI(void)
     TRISBbits.TRISB14 = 1; // Set SCK1 (RB14) as input
     ANSELBbits.ANSB14 = 0; // Disable analog function on SCK1
     TRISBbits.TRISB4 = 1;  // Set SS1 (RB4) as input
-    
-    
+
     // Step 2: Disable SPI Module
     SPI1CONbits.ON = 0;
 
@@ -89,7 +87,7 @@ void InitSPI(void)
     SPI1CONbits.MSTEN = 0; // Slave mode
     SPI1CONbits.CKE = 0;   // Reads on 2nd edge
     SPI1CONbits.CKP = 1;   // SCK idles high
-    //SPI1CONbits.SSEN = 1;  // Enable SS pin
+    // SPI1CONbits.SSEN = 1;  // Enable SS pin
 
     // Configure SPI for 8-bit communication
     SPI1CONbits.MODE16 = 0; // 8-bit mode
@@ -109,19 +107,17 @@ void InitSPI(void)
 void InitSPI_v2(void)
 {
     // Step 0: Disable analog function on all SPI pins
-    
+
     // Step 1: Map SPI Inputs/Outputs to all desired pins
-    //TRISBbits.TRISB13 = 1;  // Set SDI2 (RB13) as input
-    //ANSELBbits.ANSB13 = 0;  // Disable analog function on SDI2
-    //SDI2R = 0b0011;        // Map SDI2 to RB13
-    TRISAbits.TRISA2 = 1;  // Set SDI2 (RA2) as input
-    SDI2R = 0b0000;        // Map SDI2 to RA2
+    TRISBbits.TRISB13 = 1; // Set SDI2 (RB13) as input
+    ANSELBbits.ANSB13 = 0; // Disable analog function on SDI2
+    SDI2R = 0b0011;        // Map SDI2 to RB13
     TRISBbits.TRISB8 = 0;  // Set SDO2 (RB8) as output
     RPB8R = 0b0100;        // Map SDO2 to RB8
     TRISBbits.TRISB15 = 1; // Set SCK2 (RB15) as input
     TRISBbits.TRISB4 = 1;  // Set SS2 (RB4) as input
     ANSELBbits.ANSB15 = 0; // Disable analog function on SCK2
-    //ANSELBbits.ANSB4 = 0;  // Disable analog function on SS2
+    // ANSELBbits.ANSB4 = 0;  // Disable analog function on SS2
 
     // Step 2: Disable SPI Module
     SPI2CONbits.ON = 0;
@@ -141,7 +137,7 @@ void InitSPI_v2(void)
     SPI2CONbits.MSTEN = 0; // Slave mode
     SPI2CONbits.CKE = 0;   // Reads on 2nd edge
     SPI2CONbits.CKP = 1;   // SCK idles high
-    //SPI2CONbits.SSEN = 1;  // Enable SS pin
+    // SPI2CONbits.SSEN = 1;  // Enable SS pin
 
     // Step 8: Initialize Interrupts
     SPI2CONbits.SRXISEL = 0b01; // Interrupt when buffer is full
@@ -152,12 +148,10 @@ void InitSPI_v2(void)
     // Step 9: Enable SPI
     SPI2CONbits.ON = 1;
 
-
     SPI1CONbits.MODE16 = 0; // Enable 8 bit transfers
-    SPI1CONbits.MODE32 = 0; 
+    SPI1CONbits.MODE32 = 0;
     lastReceiveTime = ES_Timer_GetTime();
 }
-
 
 // void __ISR(_SPI_1_VECTOR, IPL6SOFT) SPIFollowerISR(void) {
 //     uint8_t receivedByte = SPI1BUF;
@@ -213,54 +207,59 @@ void InitSPI_v2(void)
 //     // Always update status for next transfer
 //     SPI1BUF = CurrentStatus;
 // }
-void __ISR(_SPI_2_VECTOR, IPL6SOFT) SPIFollowerISR(void) {
+void __ISR(_SPI_2_VECTOR, IPL6SOFT) SPIFollowerISR(void)
+{
     uint8_t receivedByte = SPI2BUF;
     IFS1CLR = _IFS1_SPI2RXIF_MASK; // Clear the interrupt flag
 
     DB_printf("Received byte: %d\r\n", receivedByte); // Add debug print
 
     // Process command directly
-    if(receivedByte >= NAV_CMD_MOVE && receivedByte <= NAV_CMD_TURN_360) {
+    if (receivedByte >= NAV_CMD_MOVE && receivedByte <= NAV_CMD_TURN_360)
+    {
         ES_Event_t CmdEvent;
         CmdEvent.EventType = ES_NEW_PLANNER_CMD;
         CmdEvent.EventParam = receivedByte;
         DB_printf("Received command: %d\r\n", receivedByte);
         PostNavigatorHSM(CmdEvent);
-    } else if (receivedByte == NAV_CMD_QUERY_STATUS) {
+    }
+    else if (receivedByte == NAV_CMD_QUERY_STATUS)
+    {
         // Update status based on Navigator state
         DB_printf("Received status query\r\n");
         NavigatorState_t currentState = QueryNavigatorHSM();
-        switch (currentState) {
-            case Init:
-                CurrentStatus = NAV_STATUS_INIT;
-                break;
-            case Idle:
-                CurrentStatus = NAV_STATUS_IDLE;
-                break;
-            case LineFollow:
-                CurrentStatus = NAV_STATUS_LINE_FOLLOW;
-                break;
-            case AlignBeacon:
-                CurrentStatus = NAV_STATUS_ALIGN_BEACON;
-                break;
-            case CheckIntersection:
-                CurrentStatus = NAV_STATUS_CHECK_INTERSECTION;
-                break;
-            case TurnLeft:
-                CurrentStatus = NAV_STATUS_TURN_LEFT;
-                break;
-            case TurnRight:
-                CurrentStatus = NAV_STATUS_TURN_RIGHT;
-                break;
-            case LineDiscover:
-                CurrentStatus = NAV_STATUS_LINE_DISCOVER;
-                break;
-            case CheckCrate:
-                CurrentStatus = NAV_STATUS_CHECK_CRATE;
-                break;
-            default:
-                CurrentStatus = NAV_STATUS_ERROR;
-                break;
+        switch (currentState)
+        {
+        case Init:
+            CurrentStatus = NAV_STATUS_INIT;
+            break;
+        case Idle:
+            CurrentStatus = NAV_STATUS_IDLE;
+            break;
+        case LineFollow:
+            CurrentStatus = NAV_STATUS_LINE_FOLLOW;
+            break;
+        case AlignBeacon:
+            CurrentStatus = NAV_STATUS_ALIGN_BEACON;
+            break;
+        case CheckIntersection:
+            CurrentStatus = NAV_STATUS_CHECK_INTERSECTION;
+            break;
+        case TurnLeft:
+            CurrentStatus = NAV_STATUS_TURN_LEFT;
+            break;
+        case TurnRight:
+            CurrentStatus = NAV_STATUS_TURN_RIGHT;
+            break;
+        case LineDiscover:
+            CurrentStatus = NAV_STATUS_LINE_DISCOVER;
+            break;
+        case CheckCrate:
+            CurrentStatus = NAV_STATUS_CHECK_CRATE;
+            break;
+        default:
+            CurrentStatus = NAV_STATUS_ERROR;
+            break;
         }
     }
 
@@ -268,7 +267,8 @@ void __ISR(_SPI_2_VECTOR, IPL6SOFT) SPIFollowerISR(void) {
     SPI2BUF = 0x02;
 }
 
-void TriggerSPIFollowerISR(void) {
+void TriggerSPIFollowerISR(void)
+{
     IFS1SET = _IFS1_SPI2RXIF_MASK; // Set the interrupt flag to trigger the ISR
-    //IFS1SET = _IFS1_SPI1RXIF_MASK;
+    // IFS1SET = _IFS1_SPI1RXIF_MASK;
 }
